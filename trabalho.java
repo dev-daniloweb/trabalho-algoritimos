ae530b3e758346008f5c4e60480e6de3354edc88
dev-daniloweb <47611020+dev-daniloweb@users.noreply.github.com>
2019-05-21 13:21:00 -0300

    Create trabalho.java

---
+++ trabalho.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class trabalho {
+
+	public static void main(String[] args) {
+		
+		int vetor[] = new int[10], num;
+		String saida = "";
+		
+		for (int i = 0; i < vetor.length; i++) {
+			num = Integer.parseInt(JOptionPane.showInputDialog("Digite o " + (i + 1) + "° número:"));
+			vetor[i] = (int)Math.pow(num, 3);
+			saida += num + "^3 = " + vetor[i] + "\n";
+		}
+		
+		JOptionPane.showMessageDialog(null, saida);
+
+	}
+
+}
